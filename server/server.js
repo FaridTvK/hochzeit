0ee57ebf13ac944ea549d077a7dad96a29dc67e8
FaridTvK <faridztvki@gmail.com>
2023-05-30 11:43:53 +0200

    done

---
+++ server/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors"
+
+import { todos, addTodo, deleteTodo, updateTodo } from "./model/TodoModel.js"
+
+
+const app = express()
+
+// es wird als default Port eingesetz, falls der Port nicht funktioniert hat.
+const PORT = process.env.Port || 3001;
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/todos", (req, res) => {
+    res.send(todos)
+})
+
+app.post("/todos", async (req, res) => {
+    const todo = req.body
+    const newTodo = await addTodo(todo)
+    res.send(newTodo)
+})
+
+app.put("/todos/:id", async (req, res) => {
+    const { id } = req.params;
+    const todo = req.body
+    const updatedTodo = await updateTodo(id, todo)
+    res.send(updatedTodo)
+})
+
+app.delete("/todos/:id", (req, res) => {
+    const { id } = req.params;
+    deleteTodo(id)
+    res.send("it has deleted")
+})
+
+app.listen(PORT, () => console.log(`Server is am laufen mit diesem Port ${PORT}`))
+
